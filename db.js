9614b857db0460dbc547b91066a344caab1a9908
Fikson211 <fikson21@icloud.com>
2026-01-25 17:02:32 +0300

    Support Railway DATABASE_URL var

--- db.js
+++ db.js
@@ -1,10 +1,15 @@
 const { Pool } = require('pg');
 
+const connectionString =
+  process.env.DATABASE_URL || process.env.DATABASE_URL;
+
+console.log('DATABASE_URL exists?', !!process.env.DATABASE_URL);
+console.log('DATABASE_URL exists?', !!process.env.DATABASE_URL);
+console.log('Using DB var:', process.env.DATABASE_URL ? 'DATABASE_URL' : (process.env.DATABASE_URL ? 'DATABASE_URL' : 'NONE'));
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString,
   ssl: { rejectUnauthorized: false },
 });
 
 module.exports = { pool };
-
-console.log("DATABASE_URL exists?", !!process.env.DATABASE_URL);
